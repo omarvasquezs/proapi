eb9955e4170fe4ad353b6755c154315247a9ecfd
Omar Vásquez <omarvs91@gmail.com>
2025-05-24 19:58:37 -0500

    Refactoring UserController to use Illuminate\Routing\Controller instead of App\Http\Controllers\Controller.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use App\Http\Resources\UserResource;
 use App\Http\Resources\UserCollection;
 use App\Models\User;
